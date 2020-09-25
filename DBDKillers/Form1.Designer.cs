04b1aadc80b54a05fa88181ead98d66d2a00371a
Luka <36271279+NotJ3st3r@users.noreply.github.com>
2020-09-25 20:57:11 +0000

    Add files via upload

---
+++ DBDKillers/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace DBDKillers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_chooseRDMKiller = new System.Windows.Forms.Button();
+            this.lbl_killerName = new System.Windows.Forms.Label();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.lbl_killerNickname = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_chooseRDMKiller
+            // 
+            this.btn_chooseRDMKiller.BackColor = System.Drawing.Color.Black;
+            this.btn_chooseRDMKiller.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_chooseRDMKiller.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_chooseRDMKiller.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_chooseRDMKiller.ForeColor = System.Drawing.Color.White;
+            this.btn_chooseRDMKiller.Location = new System.Drawing.Point(12, 446);
+            this.btn_chooseRDMKiller.Name = "btn_chooseRDMKiller";
+            this.btn_chooseRDMKiller.Size = new System.Drawing.Size(263, 48);
+            this.btn_chooseRDMKiller.TabIndex = 0;
+            this.btn_chooseRDMKiller.Text = "Randomize Killer";
+            this.btn_chooseRDMKiller.UseVisualStyleBackColor = false;
+            this.btn_chooseRDMKiller.Click += new System.EventHandler(this.Btn_chooseRDMKiller_Click);
+            // 
+            // lbl_killerName
+            // 
+            this.lbl_killerName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_killerName.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_killerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbl_killerName.ForeColor = System.Drawing.Color.Silver;
+            this.lbl_killerName.Location = new System.Drawing.Point(12, 358);
+            this.lbl_killerName.Name = "lbl_killerName";
+            this.lbl_killerName.Size = new System.Drawing.Size(263, 23);
+            this.lbl_killerName.TabIndex = 1;
+            this.lbl_killerName.Text = "label1";
+            this.lbl_killerName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pBar
+            // 
+            this.pBar.BackColor = System.Drawing.Color.Maroon;
+            this.pBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pBar.Location = new System.Drawing.Point(12, 417);
+            this.pBar.Maximum = 50;
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(263, 23);
+            this.pBar.Step = 1;
+            this.pBar.TabIndex = 2;
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox.BackColor = System.Drawing.Color.Transparent;
+            this.picBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picBox.ErrorImage = null;
+            this.picBox.InitialImage = null;
+            this.picBox.Location = new System.Drawing.Point(12, 12);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(263, 290);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox.TabIndex = 3;
+            this.picBox.TabStop = false;
+            // 
+            // lbl_killerNickname
+            // 
+            this.lbl_killerNickname.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_killerNickname.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_killerNickname.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.lbl_killerNickname.ForeColor = System.Drawing.Color.Silver;
+            this.lbl_killerNickname.Location = new System.Drawing.Point(12, 313);
+            this.lbl_killerNickname.Name = "lbl_killerNickname";
+            this.lbl_killerNickname.Size = new System.Drawing.Size(263, 45);
+            this.lbl_killerNickname.TabIndex = 4;
+            this.lbl_killerNickname.Text = "label1";
+            this.lbl_killerNickname.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(287, 506);
+            this.Controls.Add(this.lbl_killerNickname);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.lbl_killerName);
+            this.Controls.Add(this.btn_chooseRDMKiller);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "DeathByDaylight Random Killer";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_chooseRDMKiller;
+        private System.Windows.Forms.Label lbl_killerName;
+        private System.Windows.Forms.ProgressBar pBar;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Label lbl_killerNickname;
+    }
+}
+
---
+++ DBDKillers/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBDKillers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            lbl_killerNickname.Text = "";
+            lbl_killerName.Text = "";
+        }
+
+        private readonly string[,] _killers = {
+            {"0", "Evan MacMillan", "Trapper"},
+            {"1", "Philip Ojomo", "Wraith"},
+            {"2", "Max Thompson Jr.", "Hillbilly"},
+            {"3", "Sally Smithson", "Nurse"},
+            {"4", "Michael Myers", "Shape"},
+            {"5", "Lisa Sherwood", "Hag"},
+            {"6", "Herman Carter", "Doctor"},
+            {"7", "Anna", "Huntress"},
+            {"8", "Bubba Sawyer", "Cannibal"},
+            {"9", "Freddy Krueger", "Nightmare"},
+            {"10", "Amanda Young", "Pig"},
+            {"11", "Jeffrey Hawk", "Clown"},
+            {"12", "Rin Yamaoka", "Spirit"},
+            {"13", "Frank, Julie, Susie, Joey", "Legion"},
+            {"14", "Adiris", "Plague"},
+            {"15", "Danny Johnson", "Ghost Face"},
+            {"16", "Demogorgon", "Demogorgon"},
+            {"17", "Kazan Yamaoka", "Oni"},
+            {"18", "Caleb Quinn", "Deathslinger"},
+            {"19", "Pyramid Head", "Executioner"},
+            {"20", "Talbot Grimes", "Blight"}
+        };
+
+        Random RDM = new Random();
+
+        private async void Btn_chooseRDMKiller_Click(object sender, EventArgs e)
+        {
+            pBar.Value = 0;
+            int delay = RDM.Next(10, 50);
+            pBar.Maximum = delay;
+            await RdmKiller(delay);
+            int rdmKiller = RDM.Next(21);
+
+            lbl_killerName.Text = _killers[rdmKiller, 1];
+            lbl_killerNickname.Text = _killers[rdmKiller, 2];
+            switch (_killers[rdmKiller, 2])
+            {
+                case "Trapper":
+                    picBox.Image = Properties.Resources.Trapper;
+                    break;
+                case "Wraith":
+                    picBox.Image = Properties.Resources.Wraith;
+                    break;
+                case "Hillbilly":
+                    picBox.Image = Properties.Resources.Hillbilly;
+                    break;
+                case "Nurse":
+                    picBox.Image = Properties.Resources.Nurse;
+                    break;
+                case "Shape":
+                    picBox.Image = Properties.Resources.Shape;
+                    break;
+                case "Hag":
+                    picBox.Image = Properties.Resources.Hag;
+                    break;
+                case "Doctor":
+                    picBox.Image = Properties.Resources.Doctor;
+                    break;
+                case "Huntress":
+                    picBox.Image = Properties.Resources.Huntress;
+                    break;
+                case "Cannibal":
+                    picBox.Image = Properties.Resources.Cannibal;
+                    break;
+                case "Nightmare":
+                    picBox.Image = Properties.Resources.Nightmare;
+                    break;
+                case "Pig":
+                    picBox.Image = Properties.Resources.Pig;
+                    break;
+                case "Clown":
+                    picBox.Image = Properties.Resources.Clown;
+                    break;
+                case "Spirit":
+                    picBox.Image = Properties.Resources.Spirit;
+                    break;
+                case "Legion":
+                    picBox.Image = Properties.Resources.Legion;
+                    break;
+                case "Plague":
+                    picBox.Image = Properties.Resources.Plague;
+                    break;
+                case "Ghost Face":
+                    picBox.Image = Properties.Resources.Ghost_Face;
+                    break;
+                case "Demogorgon":
+                    picBox.Image = Properties.Resources.Demogorgon;
+                    break;
+                case "Oni":
+                    picBox.Image = Properties.Resources.Oni;
+                    break;
+                case "Deathslinger":
+                    picBox.Image = Properties.Resources.Deathslinger;
+                    break;
+                case "Executioner":
+                    picBox.Image = Properties.Resources.Executioner;
+                    break;
+                case "Blight":
+                    picBox.Image = Properties.Resources.Blight;
+                    break;
+
+            }
+        }
+
+        private async Task RdmKiller(int delay)
+        {
+            for (int i = 0; i < delay; i++)
+            {
+                int rdmKiller = RDM.Next(21);
+
+                lbl_killerName.Text = _killers[rdmKiller, 1];
+                lbl_killerNickname.Text = _killers[rdmKiller, 2];
+                switch (_killers[rdmKiller, 2])
+                {
+                    case "Trapper":
+                        picBox.Image = Properties.Resources.Trapper;
+                        break;
+                    case "Wraith":
+                        picBox.Image = Properties.Resources.Wraith;
+                        break;
+                    case "Hillbilly":
+                        picBox.Image = Properties.Resources.Hillbilly;
+                        break;
+                    case "Nurse":
+                        picBox.Image = Properties.Resources.Nurse;
+                        break;
+                    case "Shape":
+                        picBox.Image = Properties.Resources.Shape;
+                        break;
+                    case "Hag":
+                        picBox.Image = Properties.Resources.Hag;
+                        break;
+                    case "Doctor":
+                        picBox.Image = Properties.Resources.Doctor;
+                        break;
+                    case "Huntress":
+                        picBox.Image = Properties.Resources.Huntress;
+                        break;
+                    case "Cannibal":
+                        picBox.Image = Properties.Resources.Cannibal;
+                        break;
+                    case "Nightmare":
+                        picBox.Image = Properties.Resources.Nightmare;
+                        break;
+                    case "Pig":
+                        picBox.Image = Properties.Resources.Pig;
+                        break;
+                    case "Clown":
+                        picBox.Image = Properties.Resources.Clown;
+                        break;
+                    case "Spirit":
+                        picBox.Image = Properties.Resources.Spirit;
+                        break;
+                    case "Legion":
+                        picBox.Image = Properties.Resources.Legion;
+                        break;
+                    case "Plague":
+                        picBox.Image = Properties.Resources.Plague;
+                        break;
+                    case "Ghost Face":
+                        picBox.Image = Properties.Resources.Ghost_Face;
+                        break;
+                    case "Demogorgon":
+                        picBox.Image = Properties.Resources.Demogorgon;
+                        break;
+                    case "Oni":
+                        picBox.Image = Properties.Resources.Oni;
+                        break;
+                    case "Deathslinger":
+                        picBox.Image = Properties.Resources.Deathslinger;
+                        break;
+                    case "Executioner":
+                        picBox.Image = Properties.Resources.Executioner;
+                        break;
+                    case "Blight":
+                        picBox.Image = Properties.Resources.Blight;
+                        break;
+                }
+                pBar.PerformStep();
+                await Task.Delay(50);
+            }
+        }
+    }
+}
